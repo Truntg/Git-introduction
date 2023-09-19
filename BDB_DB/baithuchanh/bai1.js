021e94985d80fb8f2c7b383f2764821d20bf1f93
Nguyen thanh trung <trungfbi1@gmail.com>
2023-09-19 09:32:49 +0700

    JSNangCao

---
+++ BDB_DB/baithuchanh/bai1.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+async function fetchPosts(){
+    const result = await axios.get("http://jsonplaceholder.typicode.com/posts");
+    console.log("result", result);
+}
+fetchPosts();
