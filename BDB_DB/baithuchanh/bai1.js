3cbd0f9e2311335a89a6aa0e93bed54d08b6fd40
Nguyen thanh trung <trungfbi1@gmail.com>
2023-09-21 20:33:49 +0700

    batdongbo

--- BDB_DB/baithuchanh/bai1.js
+++ BDB_DB/baithuchanh/bai1.js
@@ -1,7 +1,18 @@
+// import axios from "axios";
+
+// async function fetchPosts(){
+//     const result = await axios.get("http://jsonplaceholder.typicode.com/posts");
+//     console.log("result", result);
+// }
+// fetchPosts();
 import axios from "axios";
 
-async function fetchPosts(){
-    const result = await axios.get("http://jsonplaceholder.typicode.com/posts");
-    console.log("result", result);
+async function fetchPokemon(){
+    const response = await  axios.get('https://pokeapi.co/api/v2/pokemon');
+
+    // console.log("response", response.data);
+
+    return response.data;
 }
-fetchPosts();
+
+fetchPokemon();
